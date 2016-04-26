08660efe2f37ed004f5671a127339b6cf47dce25
tang <tangdream@163.com>
2016-04-26 11:32:54 +0800

    创建工程

---
+++ drawProgressAnimation/drawProgressAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  drawProgressAnimation
+//
+//  Created by tang on 16/4/26.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ drawProgressAnimation/drawProgressAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  drawProgressAnimation
+//
+//  Created by tang on 16/4/26.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
